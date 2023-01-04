16b8e1565cf558785d0144858e6a27cee757d4d8
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2023-01-04 14:33:00 +0200

    on class

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,27 +1,62 @@
+import { Component } from "react";
+
 import "./employees-add-form.css";
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Pridėti naują darbuotoją</h3>
-			<form className="add-form d-flex">
-				<input
-					type="text"
-					className="form-control new-post-label"
-					placeholder="Vardas, Pavardė?"
-				/>
-				<input
-					type="number"
-					className="form-control new-post-label"
-					placeholder="Alga EU-ais?"
-				/>
+class EmployeesAddForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			salary: "",
+		};
+	}
 
-				<button type="submit" className="btn btn-outline-light">
-					Pridėti
-				</button>
-			</form>
-		</div>
-	);
-};
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.onAdd(this.state.name, this.state.salary);
+		this.setState({
+			name: "",
+			salary: "",
+		});
+	};
+
+	render() {
+		const { name, salary } = this.state;
+
+		return (
+			<div className="app-add-form">
+				<h3>Pridėti naują darbuotoją</h3>
+				<form className="add-form d-flex">
+					<input
+						type="text"
+						className="form-control new-post-label"
+						placeholder="Vardas, Pavardė?"
+						name="name"
+						value={name}
+						onChange={this.onValueChange}
+					/>
+					<input
+						type="number"
+						className="form-control new-post-label"
+						placeholder="Atlyginimas EU?"
+						name="salary"
+						value={salary}
+						onChange={this.onValueChange}
+					/>
+
+					<button type="submit" className="btn btn-outline-light">
+						Pridėti
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default EmployeesAddForm;
